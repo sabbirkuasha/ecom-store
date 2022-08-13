0934c6eb8a0795bd851cfd9b4c7c2613c7f5de87
sabbirz <sabbir.kuasha@gmail.com>
2022-08-14 05:23:34 +0600

    Learning function invoke and modifier
    Learning function invoking and function modifier of svelte

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Svelte-kit Ecommerce Learning [Demo](https://zoetrope.biz)
+# Svelte-kit Ecommerce Learning [Demo](https://ecom-store-peach.vercel.app/)
 
 <a href="https://www.youtube.com/watch?v=CAPb6Inm65g&list=PLtgYhHmUIr3qDB2eTzY-nuBH1W5tOK8a4&index=3"> Tutorial that I followed </a>
 
---
+++ src/routes/index2.svelte
@@ -0,0 +1,21 @@
+<script>
+    function handleClick(string){
+        alert(string)
+    }
+</script>
+
+<main class="text-center p-10">
+    <button class="btn btn-primary"
+            on:click={()=>handleClick('something')}>
+        Click Me!
+    </button>
+
+    <div    class="mt-10 w-full h-20 border bg-green-900"
+            on:click|self|once={()=>handleClick('Green Clicked')}>
+        Click Green
+        <div    class="w-1/2 m-auto p-10 border bg-red-900"
+                >
+            You can't click the red
+        </div>
+    </div>
+</main>
