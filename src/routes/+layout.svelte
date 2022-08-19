a04e60050dc6ad9599d47784b7fca53e492bb2f5
sabbirz <sabbir.kuasha@gmail.com>
2022-08-19 21:56:52 +0600

    Footer update and fliter wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
-	import '../app.postcss';
-	import NavBar from '../lib/NavBar.svelte';
+	import '../app.postcss'
+	import NavBar from '../lib/NavBar.svelte'
+	import Footer from '../lib/Common/Footer.svelte'
 
 	// import for experiment only
-	import Nav from '../lib/Nav.svelte';
-	import Nav2 from '../lib/Nav2.svelte';
-	import Nav3 from '../lib/Nav3.svelte';
+	// import Nav from '../lib/Nav.svelte'
+	// import Nav2 from '../lib/Nav2.svelte'
+	// import Nav3 from '../lib/Nav3.svelte'
 
 </script>
 
@@ -13,7 +14,7 @@
 <NavBar />
 
 <slot />
-
+<Footer/>
 <!-- <Nav />
 <Nav2 /> -->
 <!-- <Nav3 /> -->
--- src/routes/Women/Salwar-Kameez/+page.svelte
+++ src/routes/Women/Salwar-Kameez/+page.svelte
@@ -5,16 +5,26 @@
 
 <CategoryBar categoryName = "SALWAR KAMEEJ"/>
 
+<!-- Breadcrumb | Sub-Categories | Filter  -->
+<div class="w-full bg-white
+            grid grid-cols-3  md:grid-cols-2 lg:grid-cols-3 gap-1">
 
-<div class="w-full bg-white ring-2
-            grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-1">
-    <div class="w-auto bg-green-900">
-        Left Elements
+    <!-- Left - breadcrumb -->
+    <div class="w-auto order-1 col-span-2 pl-5 md:col-span-1 lg:col-span-1">
+        <div class="text-xs breadcrumbs lg:inline-flex">
+            <ul>
+                <li><a href="/">Home</a></li> 
+                <li><a href="/">Women</a></li> 
+                <li class="font-bold text-red-700">Salwar-Kameez</li>
+            </ul>
+        </div>
     </div>
-    <div class="w-auto flex">
+
+    <!-- Middle - Sub-Categories Link -->
+    <div class="w-auto flex order-3 lg:order-2 col-span-3 lg:col-span-1 align-middle">
         <!-- middle  Sub category -->
         <div class="text-black m-auto flex ">
-            <ul class="flex flex-row flex-grow justify-center text-sm">
+            <ul class="flex flex-row flex-grow justify-center text-xs">
                 <li><a class="font-bold text-red-700" href="/">Cotton</a></li>
                 <li class="px-2">|</li>
                 <li><a href="/">Silk</a></li>
@@ -29,12 +39,35 @@
             </ul>
         </div>
     </div>
-    <div class="w-auto bg-red-900">
-        Riight Elements
+
+    <!-- right - Filters -->
+    <div class="w-auto order-2 flex justify-end pr-3">
+        <!--1st Element -Filter -->
+        <div class="text-black p-2 text-right md:flex justify-end space-x-7 text-sm">
+            <button class="flex flex-row" href="/Women/Salwar-Kameez">
+               <span class="m-auto hidden sm:flex"> Filters </span>
+               <span class="m-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
+                  </svg>
+               </span>
+            </button>
+        </div>
+        <!--2nd Element - Sort By -->
+        <div class="text-black p-2 text-right md:flex justify-end space-x-7 text-sm">
+            <button class="flex flex-row" href="/Women/Salwar-Kameez">
+               <span class="m-auto hidden sm:flex"> Sort-by </span>
+               <span class="m-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z" />
+            </svg>
+               </span>
+            </button>
+        </div>
     </div>
 </div>
 
-<div class="p-4 bg-white ring-2 
+<div class="p-4 bg-white 
             grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
     <SingleProduct  product ="Chuniya"
                     price =542/>
