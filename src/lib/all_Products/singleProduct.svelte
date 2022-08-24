8068c2bf40aa69245c4b50f4b225fec4bedc5f5d
sabbirz <sabbir.kuasha@gmail.com>
2022-08-25 05:57:26 +0600

    product transition fixed

--- src/lib/all_Products/singleProduct.svelte
+++ src/lib/all_Products/singleProduct.svelte
@@ -14,14 +14,23 @@
     }
 </script>
 <div class="card w-auto shadow-md rounded-md">
-    <figure class="">
+    <figure class="relative">
 
         <!-- only on this when New badge is switched off -->
         <!-- <div class="w-12 h-12 bg-red-700 absolute left-0 bottom-0 ml-4 mb-20 flex">
             <span class="m-auto text-white text-center">NEW</span> 
         </div> -->
-       
-        <img class="w-auto h-auto" on:mouseenter={mouseEnterF} on:mouseleave={mouseLeaveF} src={curImg} alt="Shoes" />
+
+       <!-- Approach 1 -->
+        <!-- <img class="w-auto h-auto" on:mouseenter={mouseEnterF} on:mouseleave={mouseLeaveF} src={curImg} alt="Shoes" /> -->
+
+        <!-- Approach 2 -->
+        <img class="cursor-pointer absolute top-0 left-0 transition-opacity duration-250 ease-in opacity-100 hover:opacity-0 " 
+                src={img} alt="Workplace" />
+
+        <img class="cursor-pointer w-auto h-auto" 
+            src={img2} alt="Workplace"/>
+
     </figure>
     <div class=" ml-2 mt-2 rounded-xl">
         <!-- only on this when New Overlay box is switched off -->
